d0c27ab7fa1107fcb8b063b8b0608955ca46b5a0
Nadir Hamid <matrix.nad@gmail.com>
2021-11-28 01:38:47 +0000

    add updater

--- main.go
+++ main.go
@@ -8,12 +8,20 @@ import (
     "net"
     "sync"
 "time"
+"context"
+"errors"
    	"encoding/json"
     lineblocs "bitbucket.org/infinitet3ch/lineblocs-go-helpers"
 	"strings"
 	"io/ioutil"
 	"strconv"
 	"github.com/nadirhamid/amigo"
+     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/tools/clientcmd"
+    metrics "k8s.io/metrics/pkg/client/clientset/versioned"
+	"k8s.io/client-go/kubernetes"
+    	restclient "k8s.io/client-go/rest"
+    v1 "k8s.io/api/core/v1"
 
 )
 
@@ -28,7 +36,7 @@ var servers []*lineblocs.MediaServer
 var router *lineblocs.SIPRouter
 var data *ServerData
 var channel chan []*lineblocs.MediaServer
-var myServer  *lineblocs.MediaServer
+var currentNode  *lineblocs.MediaServer
 var ami  *amigo.Amigo
 
 
@@ -92,6 +100,113 @@ func (m MyBroadcastListener) OnBroadcast(b *smudge.Broadcast) {
         string(b.Bytes()))
 }
 
+func createK8sConfig() (kubernetes.Interface, *restclient.Config, error) {
+	var kubeconfig string
+	kubeconfig= "/root/.kube/config"
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+	return clientset, config,nil
+}
+func GetNodeByName(clientset kubernetes.Interface, name string, namespace string) (*v1.Node, error) {
+ 	nodes, err := ListNodes(clientset)
+    if err != nil {
+        return nil, err
+    }
+	for _, item := range nodes {
+		if item.Name == name {
+			return &item, nil
+		}
+	}
+	return nil, errors.New("No node found.")
+}
+
+func ListNodes(clientset kubernetes.Interface) ([]v1.Node, error) {
+    nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+
+    if err != nil {
+        return nil, err
+    }
+
+    return nodes.Items, nil
+}
+func addNodeToCluster() (error) {
+    fmt.Println("adding new node to cluster..")
+    return nil
+}
+func removeNode() (error) {
+    return nil
+}
+func getCPUSample2() (float64, error) {
+    clientset,config,err :=createK8sConfig()
+    if err != nil {
+        return 0,err
+    }
+    mc, err := metrics.NewForConfig(config)
+    if err != nil {
+        return 0,err
+    }
+    ctx := context.Background()
+    //ns :="voip"
+    nodeMetrics, err := mc.MetricsV1beta1().NodeMetricses().List(ctx,metav1.ListOptions{})
+    if err != nil {
+        fmt.Println("Error:", err)
+        return 0,err
+    }
+    for _, nodeMetric := range nodeMetrics.Items {
+        cpuQuantity:= nodeMetric.Usage.Cpu().MilliValue()
+        memQuantity, ok := nodeMetric.Usage.Memory().AsInt64()
+        if !ok {
+        fmt.Println("Cannot get stats..")
+            return 0,errors.New("error stats")
+        }
+        node, err := GetNodeByName(clientset, nodeMetric.Name, "voip")
+        if err != nil {
+        fmt.Println("Error:", err)
+            return 0,err
+        }
+        for _, item := range node.Status.Addresses {
+            if item.Type == "ExternalIP" {
+                ip:=item.Address
+                resavailable:=node.Status.Capacity
+               	if available, found := resavailable[v1.ResourceCPU]; found {
+                    cpuUsed := float64(cpuQuantity) / float64(available.MilliValue()) * 100
+                    if ip == os.Getenv("IP_ADDRESS") { //same node
+                        msg := fmt.Sprintf("Node Name: %s \n %f, CPU usage: %d \n Memory usage: %d", nodeMetric.Name, cpuUsed, cpuQuantity, memQuantity)
+                        fmt.Println(msg)
+                        return cpuUsed,nil
+                      } else { // another node
+                      }
+               }
+            }
+        }
+    }
+    /*
+    for _, podmetric := range podmetrics.items {
+        _=podmetric.metadata.name
+        podContainers := podMetric.Containers
+        for _, container := range podContainers {
+            cpuQuantity, ok := container.Usage.Cpu().AsInt64()
+            memQuantity, ok := container.Usage.Memory().AsInt64()
+            if !ok {
+                return 0,0
+            }
+            msg := fmt.Sprintf("Container Name: %s \n CPU usage: %d \n Memory usage: %d", container.Name, cpuQuantity, memQuantity)
+            fmt.Println(msg)
+        }
+
+    }
+    */
+    return 0,errors.New("could not get stat..")
+}
 func getCPUSample() (idle, total uint64) {
     contents, err := ioutil.ReadFile("/proc/stat")
     if err != nil {
@@ -130,26 +245,34 @@ func updateLiveStats() {
 			results := strings.Split(v, " ")
 			fmt.Printf("%s active calls\r\n", results[0]);
 			fmt.Printf("setting active calls  to %s\r\n",results[0]);
-			err = lineblocs.UpdateLiveStat(myServer, "live_call_count",  results[0])
+			err = lineblocs.UpdateLiveStat(currentNode, "live_call_count",  results[0])
 			if err != nil {
 				fmt.Println("Could not update live call count..\r\n");
 			}
 		}
 	}
 
-    idle0, total0 := getCPUSample()
-    time.Sleep(3 * time.Second)
-    idle1, total1 := getCPUSample()
+    total0,err := getCPUSample2()
+    if err != nil {
+        fmt.Println("Error: ", err)
+        return
+    }
+    if total0 > 20 { // initiate auto scale
+        addNodeToCluster()
+    } else {
+        //removeNode()
+    }
 
-    idleTicks := float64(idle1 - idle0)
-    totalTicks := float64(total1 - total0)
-    cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
-	fmt.Printf("CPU usage is at %f\r\n", cpuUsage);
-			cpuUsageStr := strconv.FormatFloat(cpuUsage, 'f', 6, 64)
-			fmt.Printf("setting active calls  to %s\r\n",cpuUsageStr)
-			err = lineblocs.UpdateLiveStat(myServer, "live_cpu_pct_used",  cpuUsageStr)
+
+    fmt.Sprintf("%f", total0)
+
+
+	fmt.Printf("CPU usage is at %f\r\n", total0);
+			cpuUsageStr := strconv.FormatFloat(total0, 'f', 6, 64)
+			fmt.Printf("setting cpu usage to %s\r\n",cpuUsageStr)
+			err = lineblocs.UpdateLiveStat(currentNode, "live_cpu_pct_used",  cpuUsageStr)
 			if err != nil {
-				fmt.Println("Could not update live call count..\r\n");
+				fmt.Println("Could not update cpu usage..\r\n");
 			}
 
 
@@ -216,9 +339,10 @@ func main() {
         //make it alive initially
         server.Status = "ALIVE"
         fmt.Printf("comparing %s and %s\r\n", server.IpAddress,os.Getenv("IP_ADDRESS"));
-        if (server.IpAddress == os.Getenv("IP_ADDRESS")) {
-            fmt.Printf("Setting my own server!\r\n");
-            myServer = server
+       	ipSlice := strings.Split(server.IpAddress, ":")
+        if (ipSlice[0] == os.Getenv("IP_ADDRESS")) {
+            fmt.Printf("Setting current node!\r\n");
+            currentNode = server
         }
 
         fmt.Printf("adding server: ID %d, IP %s with initial status %s\r\n", server.Id, server.IpAddress, server.Status)
